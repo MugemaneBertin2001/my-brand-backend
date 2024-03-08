c155d0d7c9be8d686b2b51cd3be88bdfa7701c34
Bertin M <bertin.m2001@gmail.com>
2024-03-08 22:07:13 +0200

    authentication is done

---
+++ src/models/userModel.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IUser extends Document {
+    fullName: string;
+    role: String,
+    email: String;
+    password: string;
+}
+
+const userSchema: Schema = new Schema({
+    fullName: { type: String, required: true },
+    role: {type:String, required:true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+const UserModel = mongoose.model<IUser>('User', userSchema);
+export default UserModel;
